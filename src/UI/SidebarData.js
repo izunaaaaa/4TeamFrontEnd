4a10ee263dd697783ee266fa9f01fe469ee9398b
hyeonJiP <altk5700@gmail.com>
2023-04-06 18:26:35 +0900

    hj first commit

---
+++ src/UI/SidebarData.js
@@ -0,0 +1,10 @@
+export const SidebarData = [
+  {
+    title: "test1",
+    path: "/",
+  },
+  {
+    title: "test2",
+    path: "/",
+  },
+];
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,10 +1,56 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import Feed from "./Feed";
+import { CgMenuLeft } from "react-icons/cg";
+import styles from "./Main.module.scss";
+import Sidebar from "../../UI/Sidebar";
 
 function Main(): ReactElement {
+  const [mediaWidth, setMediaWidth] = useState<number>(window.innerWidth);
+  const [sidebar, setSidebar] = useState<boolean>(false);
+
+  useEffect(() => {
+    // 창 크기가 조절될 때마다 mediaWidth 상태를 업데이트
+    const handleResize = () => {
+      setMediaWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <div>Main</div>
+      {mediaWidth > 768 ? (
+        <>
+          {/* <div className={styles.logo}>
+            <h1>CurA</h1>
+          </div> */}
+          <div className={styles.nav_bar}>
+            <h1>CurB</h1>
+            <div className={styles.myFrofile}>
+              <img src="http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"></img>
+            </div>
+          </div>
+          <Sidebar sidebar={true} />
+          <div className={styles.main}></div>
+        </>
+      ) : (
+        <>
+          <div className={styles.nav_bar}>
+            <div className={styles.sidebar_btn}>
+              <CgMenuLeft onClick={() => setSidebar(!sidebar)} />
+            </div>
+            <h1>CurB</h1>
+            <div className={styles.myFrofile}></div>
+          </div>
+          <div className={sidebar ? "nav_menu active" : "nav_menu"}>
+            <Sidebar sidebar={sidebar} />
+          </div>
+        </>
+      )}
       <Feed />
     </>
   );
