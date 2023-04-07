5044e0986a527229cb92b3eee74ca4459599e464
chhw130 <527coco@naver.com>
2023-04-07 21:17:25 +0900

    4/7

--- src/components/form/feed/ReviseFeed.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ReviseFeed = () => {
-  return <div></div>;
-};
-
-export default ReviseFeed;
--- src/pages/main/Feed.tsx
+++ src/pages/main/Feed.tsx
@@ -5,40 +5,92 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMessage, faThumbsUp } from "@fortawesome/free-regular-svg-icons";
 import { useState } from "react";
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import {
+  Avatar,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import FeedDetail from "../../components/form/feed/FeedDetail";
+import DeleteConfirm from "../../UI/DeleteConfirm";
 
 const myFeedDropDownMenu = ["수정하기", "삭제하기"];
-//
+// const otherFeedDropDownMenu = ["쪽지 보내기"];
+
 function Feed() {
   const feedData = useFeed();
+  const navigate = useNavigate();
   const [isClickLike, setIsClickLike] = useState(false);
-  const [dropDown, setDropDown] = useState(false);
+  const [isClickMenu, setIsClickMenu] = useState(false);
+  const [dropDown, setDropDown] = useState("0");
+
+  /**게시글 보기 모달 */
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [modalType, setModalType] = useState(<FeedDetail />);
 
   /**추천 클릭시 이벤트 */
-  const thumbClickHandler = (e: any) => {
-    console.log(e.target);
+  const thumbClickHandler = (e: React.MouseEvent) => {
     setIsClickLike(!isClickLike);
+  };
+
+  /**feed 드롭다운 메뉴 이벤트 */
+  const dropDownMenuEvent = (e: React.MouseEvent) => {
+    const eventTarget = e.target as HTMLElement;
+    const menuType = eventTarget.innerText;
+    setIsClickMenu(!isClickMenu);
+    if (menuType === "수정하기") return navigate("/upload");
+    if (menuType === "삭제하기") {
+      onOpen();
+      return setModalType(<DeleteConfirm onClose={onClose} />);
+    }
   };
 
   return (
     <>
-      {feedData?.map((data) => (
+      {feedData?.map((data, index) => (
         <div key={data.id} className={styles.feedDiv}>
-          <h2 className={styles.feedUser}>{data.user}</h2>
+          <h2 className={styles.feedUser}>
+            <Avatar name={data.user} size="xs" />
+            {data.user}
+          </h2>
           <div className={styles.feedMenu}>
-            <button onClick={() => setDropDown(!dropDown)}>
+            <button
+              className={styles.dropDownBtn}
+              value={data.id}
+              onClick={(e: any) => {
+                setDropDown(e.target.value);
+                console.log(e.target);
+                setIsClickMenu(!isClickMenu);
+              }}
+            >
               <FontAwesomeIcon icon={faEllipsis} size="2x" />
             </button>
-            <ul className={styles.menu}>
-              {dropDown &&
+            <ul
+              className={
+                dropDown === String(index + 1) ? styles.menu : styles.disable
+              }
+            >
+              {isClickMenu &&
                 myFeedDropDownMenu.map((menu) => (
-                  <li className={styles.menuList} key={menu}>
+                  <li
+                    className={styles.menuList}
+                    key={menu}
+                    onClick={dropDownMenuEvent}
+                  >
                     {menu}
                   </li>
                 ))}
             </ul>
           </div>
           <img src={data.medias[0]} alt="" />
-          <div>
+          <div className={styles.iconDiv}>
             <button key={data.id} onClick={thumbClickHandler}>
               {!isClickLike ? (
                 <FontAwesomeIcon icon={faThumbsUp} size="2x" />
@@ -50,14 +102,31 @@ function Feed() {
                 />
               )}
             </button>
+            <p>12</p>
             <button>
               <FontAwesomeIcon icon={faMessage} size="2x" />
             </button>
+            <p>3</p>
           </div>
 
           <p>{data.description}</p>
+          <div
+            onClick={() => {
+              setModalType(<FeedDetail />);
+              onOpen();
+            }}
+          >
+            댓글모두 보기
+          </div>
         </div>
       ))}
+
+      <Modal isOpen={isOpen} onClose={onClose} size="lg" isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalBody>{modalType}</ModalBody>
+        </ModalContent>
+      </Modal>
       <Comment />
     </>
   );
