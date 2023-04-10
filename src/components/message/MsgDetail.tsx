018b9e37ceb6daf9930359e348d6515fe63bbd68
이풍현 <expand_a@naver.com>
2023-04-10 14:19:28 +0900

    phcommit

--- src/components/message/MsgDetail.tsx
+++ src/components/message/MsgDetail.tsx
@@ -13,7 +13,6 @@ export default function MsgDetail(props: MockCont) {
       <Box
         sx={{
           width: "15rem",
-          textAlign: "end",
           my: 1,
           p: 2,
         }}
--- src/pages/messagebox/MsgRoom.tsx
+++ src/pages/messagebox/MsgRoom.tsx
@@ -12,16 +12,31 @@ import {
   ModalFooter,
   ModalBody,
   Textarea,
+  FormControl,
 } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
-import MsgDetail from "../../components/message/MsgComment";
+import { useForm } from "react-hook-form";
+import MsgDetail from "components/message/MsgDetail";
 
 export default function MsgRoom() {
+  // chakra ui의 모달 컨트롤 훅
   const { isOpen, onOpen, onClose } = useDisclosure();
+  //쪽지 내용 폼 관리
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => {
+    const sendmessage = data.message.trim();
+    if (sendmessage === "") {
+    } else {
+      console.log(data);
+      onClose();
+    }
+  };
 
   return (
     <>
+      {/* 쪽지내역 */}
       {mockMsgCont?.map((msg: MockCont, idx) => {
         return (
           <Flex key={idx} justify={"space-around"}>
@@ -32,15 +47,18 @@ export default function MsgRoom() {
       <Button onClick={onOpen}>
         <FontAwesomeIcon icon={faPaperPlane} size="2x" />
       </Button>
+      {/* 모달 */}
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>쪽지</ModalHeader>
           <ModalBody>
-            <Textarea placeholder="보내실 내용을 입력해주세요" />
+            <FormControl>
+              <Textarea placeholder="보내실 내용을 입력해주세요" />
+            </FormControl>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
+            <Button colorScheme="blue" mr={3} onClick={handleSubmit(onSubmit)}>
               Send
             </Button>
             <Button color="black" onClick={onClose}>
