8be90cbbb52e886c45dabfe15b52bc1aa62a5c51
임창섭 <ggggjkas@gmail.com>
2023-04-10 18:19:04 +0900

    mypage - 내정보 form

--- src/components/mypages/tabMenu/WrittenPost.tsx
+++ src/components/mypages/tabMenu/WrittenPost.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function WrittenPost() {
    // 게시글 데이터 타입 정의
@@ -16,42 +15,5 @@ export default function WrittenPost() {
       { id: 3, title: '세 번째 게시글', date: '2023-03-03', views: 16 },
    ];
 
-   //
-   //    /* comment에 필요한 api 호출
-   //          1. 작성글 제목
-   //          2. 작성한 날짜
-   //          3. 조회한 수?, 좋아요한 수?
-
-   //          */
-
-   return (
-      <>
-         <div>
-            <table>
-               <thead>
-                  <tr>
-                     <th>선택</th>
-                     <th>제목</th>
-                     <th>작성일</th>
-                     <th>조회수</th>
-                  </tr>
-               </thead>
-               <tbody>
-                  {posts.map(post => (
-                     <tr key={post.id}>
-                        <td>
-                           <input type='checkbox' />
-                        </td>
-                        <td>
-                           <Link to={`/posts/${post.id}`}>{post.title}</Link>
-                        </td>
-                        <td>{post.date}</td>
-                        <td>{post.views}</td>
-                     </tr>
-                  ))}
-               </tbody>
-            </table>
-         </div>
-      </>
-   );
+   return <></>;
 }
