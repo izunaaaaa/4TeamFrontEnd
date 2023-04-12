9a6fcead5e4f79b450ca1e071326913e2520295b
hyeonJiP <altk5700@gmail.com>
2023-04-12 15:07:36 +0900

    sidebar

--- src/UI/sidebar/Sidebar.tsx
+++ src/UI/sidebar/Sidebar.tsx
@@ -1,66 +1,87 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { SidebarData } from "./SidebarData";
 import styles from "./Sidebar.module.scss";
 import { SidebarProps } from "../../interface/Interface";
 import { Link } from "react-router-dom";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import { instance } from "api/axios/axiosSetting";
+
+interface Category {
+  id: number;
+  name: string;
+}
 
 function Sidebar({ sidebar, setSidebar }: SidebarProps) {
   // 새로운 채널의 이름을 저장하는 상태 변수
   const [newChannelName, setNewChannelName] = useState("");
   const [showModal, setShowModal] = useState(false);
-  const [sidebarData, setSidebarData] = useState(SidebarData);
+  //const [sidebarData, setSidebarData] = useState(SidebarData);
+  const [sidebarData, setSidebarData] = useState<Category[]>([]);
   // 관리자 여부를 나타내는 상태
   const [isAdmin, setIsAdmin] = useState(true);
 
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = async () => {
+    try {
+      const response = await instance.get("/categories/?group=oz");
+      setSidebarData(response.data);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
+  const addCategory = async (name: string) => {
+    try {
+      const response = await instance.post("/categories/?group=oz", { name });
+      setSidebarData((prevData) => [...prevData, response.data]);
+    } catch (error) {
+      console.error("Error adding category:", error);
+    }
+  };
+
   const handleAddChannel = () => {
-    // 공백이 아닐 때
     if (newChannelName.trim()) {
-      setSidebarData((prevData) => [
-        ...prevData,
-        { title: newChannelName, path: "/" },
-      ]);
+      addCategory(newChannelName);
       setNewChannelName("");
       closeModal();
     }
   };
 
-  // 카테고리 추가 모달창
-  const openModal = () => {
-    setShowModal(true);
-  };
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
 
-  const closeModal = () => {
-    setShowModal(false);
-  };
+  const renderSidebarData = () =>
+    sidebarData?.map((item, index) => (
+      <li key={index} className={styles.nav_text}>
+        <Link to={"#"}>
+          <span>{item.name}</span>
+        </Link>
+      </li>
+    ));
+
+  const renderAddChannelButton = () => (
+    <li className={styles.nav_text} onClick={openModal}>
+      <span>
+        <FontAwesomeIcon icon={faCirclePlus} size="lg" /> 채널 추가
+      </span>
+    </li>
+  );
 
   return (
     <>
-      {/* handleClickOutside 함수에서 클릭 대상과 사이드바 요소 간의 포함 관계를 비교하여 외부 클릭을 감지 */}
       <nav
         className={
           sidebar ? `${styles.nav_menu} ${styles.active}` : styles.nav_menu
         }
       >
         <ul className={styles.navmenu_items}>
-          {sidebarData.map((item, index) => {
-            return (
-              <li key={index} className={styles.nav_text}>
-                <Link to={"#"}>
-                  <span>{item.title}</span>
-                </Link>
-              </li>
-            );
-          })}
-
-          {isAdmin && (
-            <li className={styles.nav_text} onClick={openModal}>
-              <span>
-                <FontAwesomeIcon icon={faCirclePlus} size="lg" /> 채널 추가
-              </span>
-            </li>
-          )}
+          {renderSidebarData()}
+          {isAdmin && renderAddChannelButton()}
         </ul>
       </nav>
       {showModal && (
@@ -86,4 +107,5 @@ function Sidebar({ sidebar, setSidebar }: SidebarProps) {
     </>
   );
 }
+
 export default Sidebar;
