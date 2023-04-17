5324b2d0d6b64af66541661be64e9eca1baf0588
chhw130 <527coco@naver.com>
2023-04-17 16:46:19 +0900

    add: add recoil for jwt

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,68 +1,77 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignUp from 'pages/SignUp/SignUp';
-import Main from './pages/main/Main';
-import UploadFeed from './components/form/feed/UploadFeed';
-import MsgRoom from './pages/sendletters/MsgRoom';
-import Mailbox from './pages/sendletters/Mailbox';
-import Layout from './UI/Layout';
-import Feed from './pages/main/Feed';
-import WrittenPost from 'components/mypages/tabMenu/WrittenPost';
-import LikedPost from 'components/mypages/tabMenu/LikedPost';
-import MyPage from 'pages/mypage/MyPage';
-import WrittenComment from 'components/mypages/tabMenu/WrittenComment';
-import Profile from 'components/mypages/myProfile/Profiles';
-import Login from 'components/form/User/Login';
-import SignUpForm from 'components/form/User/SignUpForm';
-import SignUpMain from 'pages/SignUp/SignUpMain';
-import SignUpFormManager from 'components/form/User/SignUpFormManager';
-import Landing from 'UI/landing/Landing';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "pages/SignUp/SignUp";
+import Main from "./pages/main/Main";
+import UploadFeed from "./components/form/feed/UploadFeed";
+import MsgRoom from "./pages/sendletters/MsgRoom";
+import Mailbox from "./pages/sendletters/Mailbox";
+import Layout from "./UI/Layout";
+import Feed from "./pages/main/Feed";
+import WrittenPost from "components/mypages/tabMenu/WrittenPost";
+import LikedPost from "components/mypages/tabMenu/LikedPost";
+import MyPage from "pages/mypage/MyPage";
+import WrittenComment from "components/mypages/tabMenu/WrittenComment";
+import Profile from "components/mypages/myProfile/Profiles";
+import Login from "components/form/User/Login";
+import SignUpForm from "components/form/User/SignUpForm";
+import SignUpMain from "pages/SignUp/SignUpMain";
+import SignUpFormManager from "components/form/User/SignUpFormManager";
+import Landing from "UI/landing/Landing";
 
 const Routing = () => {
-   return (
-      <BrowserRouter>
-         <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/main' element={<Main />} />
-            <Route path='/letterlists' element={<Mailbox />} />
-            <Route path='/letters' element={<MsgRoom />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/letterlists" element={<Mailbox />} />
+        <Route path="/letters" element={<MsgRoom />} />
 
-            <Route
-               path='/home'
-               element={
-                  <Layout>
-                     <Feed />
-                  </Layout>
-               }
-            />
+        <Route
+          path="/home"
+          element={
+            <Layout>
+              <Feed />
+            </Layout>
+          }
+        />
 
-            <Route path='/curb' element={<Landing />} />
-            <Route path='/signup/' element={<SignUp />}>
-               <Route path='main' element={<SignUpMain />} />
-               <Route path='student' element={<SignUpForm />} />
-               <Route path='manager' element={<SignUpFormManager />} />
-            </Route>
-            <Route path='/upload' element={<UploadFeed />} />
+        <Route path="/curb" element={<Landing />} />
+        <Route path="/signup/" element={<SignUp />}>
+          <Route path="main" element={<SignUpMain />} />
+          <Route path="student" element={<SignUpForm />} />
+          <Route path="manager" element={<SignUpFormManager />} />
+        </Route>
 
-            {/* 내정보 */}
+        {/* 피드 업로드 */}
+        <Route
+          path="/upload"
+          element={
+            <Layout>
+              <UploadFeed />
+            </Layout>
+          }
+        />
 
-            <Route
-               path='/mypage'
-               element={
-                  <Layout>
-                     <MyPage />
-                  </Layout>
-               }
-            >
-               {/* 작성글, 작성댓글, 댓글단 글,  좋아요한 글, 삭제한 글 */}
-               <Route path='written' element={<WrittenPost />} />
-               <Route path='writtencomment' element={<WrittenComment />} />
-               <Route path='likes' element={<LikedPost />} />
-               <Route path='profile' element={<Profile />} />
-            </Route>
-         </Routes>
-      </BrowserRouter>
-   );
+        {/* 내정보 */}
+
+        <Route
+          path="/mypage"
+          element={
+            <Layout>
+              <MyPage />
+            </Layout>
+          }
+        >
+          {/* 작성글, 작성댓글, 댓글단 글,  좋아요한 글, 삭제한 글 */}
+          <Route path="written" element={<WrittenPost />} />
+          <Route path="writtencomment" element={<WrittenComment />} />
+          <Route path="likes" element={<LikedPost />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Routing;
--- src/components/form/feed/UploadFeed.tsx
+++ src/components/form/feed/UploadFeed.tsx
@@ -1,5 +1,6 @@
 import {
   Avatar,
+  Flex,
   Modal,
   ModalContent,
   ModalOverlay,
@@ -64,62 +65,67 @@ const UploadFeed = () => {
         </ModalContent>
       </Modal>
 
-      <form onSubmit={handleSubmit(submitHandler)} className={styles.postForm}>
-        <div className={styles.postFormHeader}>
-          <button onClick={() => navigate("/home")}>
-            <FontAwesomeIcon icon={faArrowLeft} />
-          </button>
-          <h3>새 게시물 작성하기</h3>
-          <button onSubmit={handleSubmit(submitHandler)}>업로드하기</button>
-        </div>
+      <Flex justifyContent="center" margin="20px">
+        <form
+          onSubmit={handleSubmit(submitHandler)}
+          className={styles.postForm}
+        >
+          <div className={styles.postFormHeader}>
+            <button onClick={() => navigate("/home")}>
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </button>
+            <h3>새 게시물 작성하기</h3>
+            <button onSubmit={handleSubmit(submitHandler)}>업로드하기</button>
+          </div>
 
-        <div className={styles.postFormMain}>
-          <div className={styles.fileForm}>
-            {!cropImg ? (
-              <>
-                <input
-                  type="file"
-                  {...register("file")}
-                  accept="image/*"
-                  onChange={changeImg}
-                />
-                <button>
-                  <FontAwesomeIcon
-                    icon={faCloudArrowUp}
-                    size="5x"
-                    style={{ color: "skyblue" }}
+          <div className={styles.postFormMain}>
+            <div className={styles.fileForm}>
+              {!cropImg ? (
+                <>
+                  <input
+                    type="file"
+                    {...register("file")}
+                    accept="image/*"
+                    onChange={changeImg}
                   />
-                </button>
-              </>
-            ) : (
-              <img alt=" " className={styles.previewImg} src={cropImg} />
-            )}
-          </div>
-
-          <div className={styles.postFormDiv}>
-            <Select
-              placeholder="카테고리를 입력해주세요"
-              size="sm"
-              {...register("category", {
-                required: true,
-              })}
-            >
-              {feedCategory.map((category: any) => {
-                return <option key={category.id}>{category.name}</option>;
-              })}
-            </Select>
-            <div className={styles.postFormNickname}>
-              <Avatar name="닉네임" size="xs" />
-              <p>닉네임</p>
+                  <button>
+                    <FontAwesomeIcon
+                      icon={faCloudArrowUp}
+                      size="5x"
+                      style={{ color: "skyblue" }}
+                    />
+                  </button>
+                </>
+              ) : (
+                <img alt=" " className={styles.previewImg} src={cropImg} />
+              )}
             </div>
-            <textarea
-              className={styles.contents}
-              placeholder="내용을 입력해주세요..."
-              {...register("content")}
-            />
+
+            <div className={styles.postFormDiv}>
+              <Select
+                placeholder="카테고리를 입력해주세요"
+                size="sm"
+                {...register("category", {
+                  required: true,
+                })}
+              >
+                {feedCategory.map((category: any) => {
+                  return <option key={category.id}>{category.name}</option>;
+                })}
+              </Select>
+              <div className={styles.postFormNickname}>
+                <Avatar name="닉네임" size="xs" />
+                <p>닉네임</p>
+              </div>
+              <textarea
+                className={styles.contents}
+                placeholder="내용을 입력해주세요..."
+                {...register("content")}
+              />
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </Flex>
     </>
   );
 };
