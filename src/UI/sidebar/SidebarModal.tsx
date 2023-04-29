496c5df10cbfe63f71a21fc5d32ac9b2445894be
hyeonJiP <altk5700@gmail.com>
2023-04-29 16:03:20 +0900

    side.search

--- src/UI/sidebar/SidebarModal.tsx
+++ src/UI/sidebar/SidebarModal.tsx
@@ -1,138 +1,24 @@
 import React, { useEffect, useState } from "react";
 import styles from "./SidebarModal.module.scss";
 
-export interface SidebarModalProps {
-  type: "add" | "edit" | "delete" | null;
-  data: any;
-  handleModal: (type: "add" | "edit" | "delete" | null, data?: any) => void;
-  handleAction: (type: "add" | "edit" | "delete", data: any) => void;
+interface SidebarModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  children: React.ReactNode;
 }
 
-const SidebarModal: React.FC<SidebarModalProps> = ({
-  type,
-  data,
-  handleModal,
-  handleAction,
-}) => {
-  const [updatedChannelName, setUpdatedChannelName] = useState("");
-
-  useEffect(() => {
-    if (!type) {
-      setUpdatedChannelName("");
-    }
-  }, [type]);
-
-  const handleSubmit = () => {
-    console.log("handleSubmit called"); // Add this line
-    if (type === "edit") {
-      handleAction(type, updatedChannelName);
-      console.log(updatedChannelName);
-    } else if (type === "add" || type === "delete") {
-      handleAction(type, data);
-    }
-  };
-
-  const renderModalContent = () => {
-    switch (type) {
-      case "add":
-        return (
-          <div className={styles.modalinner}>
-            <h2>채널 추가</h2>
-            <input
-              type="text"
-              value={data}
-              onChange={(e) => handleModal(type, e.target.value)}
-              placeholder="채널 이름 입력"
-            />
-            <div>
-              <button
-                className={styles.sideBtn_le}
-                type="button"
-                onClick={handleSubmit}
-              >
-                추가
-              </button>
-              <button
-                className={styles.sideBtn_ri}
-                type="button"
-                onClick={() => handleModal(null)}
-              >
-                취소
-              </button>
-            </div>
-          </div>
-        );
-      case "edit":
-        return (
-          <div className={styles.modalinner}>
-            <h2>채널 수정</h2>
-            <input
-              type="text"
-              value={updatedChannelName}
-              onChange={(e) => setUpdatedChannelName(e.target.value)}
-              placeholder="채널 이름 입력"
-            />
-            <div>
-              <button
-                className={styles.sideBtn_le}
-                type="button"
-                onClick={handleSubmit}
-              >
-                <p>수정</p>
-              </button>
-              <button
-                className={styles.sideBtn_ri}
-                type="button"
-                onClick={() => handleModal(null)}
-              >
-                <p>취소</p>
-              </button>
-            </div>
-          </div>
-        );
-
-      case "delete":
-        return (
-          <div className={styles.modalinner}>
-            <h2>카테고리 삭제</h2>
-            <p>정말로 삭제하시겠습니까?</p>
-            <div>
-              <button
-                className={styles.sideBtn_le}
-                type="button"
-                onClick={handleSubmit}
-              >
-                삭제
-              </button>
-              <button
-                className={styles.sideBtn_ri}
-                type="button"
-                onClick={() => handleModal(null)}
-              >
-                취소
-              </button>
-            </div>
-          </div>
-        );
-      default:
-        return null;
-    }
-  };
+function SidebarModal({ isOpen, onClose, title, children }: SidebarModalProps) {
+  if (!isOpen) return null;
 
   return (
-    <>
-      {type && (
-        <div className={styles.modal} onClick={() => handleModal(null)}>
-          <div
-            className={styles.modalContent}
-            onClick={(e) => e.stopPropagation()}
-          >
-            {renderModalContent()}
-          </div>
-        </div>
-      )}
-    </>
+    <div className={styles.modal} onClick={onClose}>
+      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+        <h2>{title}</h2>
+        {children}
+      </div>
+    </div>
   );
-};
+}
 
 export default SidebarModal;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,35 @@
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// module.exports = function (app) {
+//   app.use(
+//     createProxyMiddleware("/api/v1", {
+//       // target: "http://115.85.181.9",
+//       target: "http://127.0.0.1:8000",
+//       changeOrigin: true,
+//     })
+//   );
+//   app.use(
+//     createProxyMiddleware("/sms/v2/services", {
+//       target: "https://sens.apigw.ntruss.com",
+
+//       changeOrigin: true,
+//     })
+//   );
+// };
+
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api/v1", {
-      // target: "http://115.85.181.9",
-      target: "http://127.0.0.1:8000",
+      target: "https://backend.curb.site/",
+      // target: "https://127.0.0.1:8000/",
       changeOrigin: true,
     })
   );
   app.use(
     createProxyMiddleware("/sms/v2/services", {
-      target: "https://sens.apigw.ntruss.com",
+      target: "https://sens.apigw.ntruss.com/",
 
       changeOrigin: true,
     })
