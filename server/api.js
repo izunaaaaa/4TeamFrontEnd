6084d86d95d3ae6fff8bc98f6130a1a5a8585a33
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-04-29 22:25:00 +0900

    update : letterURL

--- server/api.js
+++ server/api.js
@@ -1,6 +1,9 @@
 const express = require(`express`);
 const app = express();
 
+app.listen(8080, function () {
+  console.log("datga");
+});
 app.get(`/api/users/:type`, async (req, res) => {
   res.send("connect");
 });
