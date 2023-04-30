46f282bfc0ae06c69fc722b4360fb4e42eeae62a
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-04-30 16:18:47 +0900

    update :component

---
+++ src/components/Card/MainFeedCard.tsx
@@ -0,0 +1,81 @@
+import { Avatar, Box, Button, Flex, HStack, Image } from "@chakra-ui/react";
+import React from "react";
+import styles from "../../pages/main/FeedPage.module.scss";
+import moment from "moment";
+import "moment/locale/ko";
+import FeedOption from "components/Feed/FeedOption";
+import LikeBtn from "UI/Button/LikeBtn";
+import { FiMessageSquare } from "react-icons/fi";
+import SendBtn from "UI/Button/SendBtn";
+import { useNavigate } from "react-router-dom";
+
+const MainFeedCard = (props: any) => {
+  const data = props.data;
+  const LoginUserData = props.LoginUserData;
+  const groupName = LoginUserData?.group.name;
+
+  const navigate = useNavigate();
+
+  const viewDetail = (dataId: number) => {
+    navigate(`feedDetail/${dataId}`);
+  };
+  return (
+    <div key={data.id} className={styles.feedDiv}>
+      <div className={styles.feedUser}>
+        <Avatar
+          name="익명"
+          size="sm"
+          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+        />
+        <Flex justifyContent="space-between" w="100%">
+          <h1>
+            <p>{groupName}의 개발자</p>
+            {moment(data.created_at).fromNow()}
+          </h1>
+          <FeedOption data={data} LoginUserData={LoginUserData} />
+        </Flex>
+      </div>
+      {data.thumbnail && (
+        <Image
+          src={data.thumbnail}
+          margin="20px 0"
+          cursor="pointer"
+          onClick={() => viewDetail(data.id)}
+        />
+      )}
+      <Box
+        marginBottom="20px"
+        fontSize="1.2rem"
+        onClick={() => viewDetail(data.id)}
+        cursor="pointer"
+      >
+        {data.title}
+      </Box>
+      <HStack spacing="1px">
+        <LikeBtn
+          id={data.id}
+          likeCount={data.like_count}
+          isLike={data.is_like}
+        />
+
+        <Button
+          padding="5px"
+          backgroundColor="transparent"
+          leftIcon={<FiMessageSquare />}
+          onClick={() => viewDetail(data.id)}
+        >
+          {data.comments_count}
+        </Button>
+        {LoginUserData?.id !== data.user?.pk && (
+          <SendBtn userPk={data.user.pk} />
+        )}
+      </HStack>
+
+      <div className={styles.comment} onClick={() => viewDetail(data.id)}>
+        댓글모두 보기
+      </div>
+    </div>
+  );
+};
+
+export default MainFeedCard;
--- src/pages/main/FeedPage.tsx
+++ src/pages/main/FeedPage.tsx
@@ -1,39 +1,20 @@
 import { useFeed } from "./hook/useFeed";
 import styles from "./FeedPage.module.scss";
-import { Outlet, useNavigate, useParams } from "react-router-dom";
-import {
-  Avatar,
-  Box,
-  Button,
-  Flex,
-  HStack,
-  Image,
-  Spinner,
-} from "@chakra-ui/react";
+import { Outlet, useParams } from "react-router-dom";
+import { Box, Spinner } from "@chakra-ui/react";
 import InfiniteScroll from "react-infinite-scroller";
-import moment from "moment";
-import "moment/locale/ko";
 import useUser from "components/form/User/Hook/useUser";
 import { DefaultFeedData } from "./interface/type";
-import FeedOption from "../../components/Feed/FeedOption";
-import { FiMessageSquare } from "react-icons/fi";
-import LikeBtn from "../../UI/Button/LikeBtn";
-import SendBtn from "UI/Button/SendBtn";
+import MainFeedCard from "components/Card/MainFeedCard";
 
 function FeedPage() {
   const { pk: groupPk, id: categoryId } = useParams();
   const { LoginUserData } = useUser();
-  const navigate = useNavigate();
 
-  const groupName = LoginUserData.group.name;
   const { feedData, fetchNextPage, hasNextPage, isFetching } = useFeed(
     groupPk,
     categoryId
   );
-
-  const viewDetail = (dataId: number) => {
-    navigate(`feedDetail/${dataId}`);
-  };
 
   return (
     <>
@@ -57,64 +38,11 @@ function FeedPage() {
           {feedData.pages?.map((pageData: any) =>
             pageData?.results?.map((data: DefaultFeedData) => {
               return (
-                <div key={data.id} className={styles.feedDiv}>
-                  <div className={styles.feedUser}>
-                    <Avatar
-                      name="익명"
-                      size="sm"
-                      src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-                    />
-                    <Flex justifyContent="space-between" w="100%">
-                      <h1>
-                        <p>{groupName}의 개발자</p>
-                        {moment(data.created_at).fromNow()}
-                      </h1>
-                      <FeedOption data={data} LoginUserData={LoginUserData} />
-                    </Flex>
-                  </div>
-                  {data.thumbnail && (
-                    <Image
-                      src={data.thumbnail}
-                      margin="20px 0"
-                      cursor="pointer"
-                      onClick={() => viewDetail(data.id)}
-                    />
-                  )}
-                  <Box
-                    marginBottom="20px"
-                    fontSize="1.2rem"
-                    onClick={() => viewDetail(data.id)}
-                    cursor="pointer"
-                  >
-                    {data.title}
-                  </Box>
-                  <HStack spacing="1px">
-                    <LikeBtn
-                      id={data.id}
-                      likeCount={data.like_count}
-                      isLike={data.is_like}
-                    />
-
-                    <Button
-                      padding="5px"
-                      backgroundColor="transparent"
-                      leftIcon={<FiMessageSquare />}
-                      onClick={() => viewDetail(data.id)}
-                    >
-                      {data.comments_count}
-                    </Button>
-                    {LoginUserData?.id !== data.user?.pk && (
-                      <SendBtn userPk={data.user.pk} />
-                    )}
-                  </HStack>
-
-                  <div
-                    className={styles.comment}
-                    onClick={() => viewDetail(data.id)}
-                  >
-                    댓글모두 보기
-                  </div>
-                </div>
+                <MainFeedCard
+                  key={data.id}
+                  data={data}
+                  LoginUserData={LoginUserData}
+                />
               );
             })
           )}
