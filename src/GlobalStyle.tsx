a69d974aaf8936cd1898ff0b40816e28a8e7cd1f
hyeonJiP <altk5700@gmail.com>
2023-04-30 18:32:17 +0900

    css

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,9 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css');
+
   * {
     margin: 0;
     padding: 0;
+  }
+  span, p, h1, h2, h3 {
+    font-family: nanumsquare;
   }
   body {
     width: 100%;
@@ -22,6 +27,5 @@ const GlobalStyle = createGlobalStyle`
   }
 
   `;
-// @import url('https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css');
 
 export default GlobalStyle;
