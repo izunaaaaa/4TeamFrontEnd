51cfce42cc4de96666ca652782411a66e0acb85c
이풍현 <expand_a@naver.com>
2023-05-01 17:08:13 +0900

    edit mobile responsive

--- src/pages/sendletters/MobileMsg.tsx
+++ src/pages/sendletters/MobileMsg.tsx
@@ -3,11 +3,11 @@ import MsgRoom from "./MsgRoom";
 import { Box } from "@chakra-ui/react";
 
 const MobileMsg = () => {
-  const isMobile = window.innerWidth < 768; // 현재 브라우저 창의 너비가 480px 이하인지 확인하는 변수
-  const ml = isMobile ? 0 : "20rem";
+  const isMobile =
+    window.innerWidth < 480 ? "0" : window.innerWidth < 769 ? "15rem" : "23rem";
 
   return (
-    <Box ml={ml}>
+    <Box ml={isMobile}>
       <MsgRoom />
     </Box>
   );
