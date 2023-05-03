0877e4860fc218ad0e4c74f0680054757768998f
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-05-03 21:35:59 +0900

    update : mypage

--- src/components/Auth/AuthPage.tsx
+++ src/components/Auth/AuthPage.tsx
@@ -1,4 +1,4 @@
-import { Spinner } from "@chakra-ui/react";
+import { Spinner, useToast } from "@chakra-ui/react";
 import useUser from "components/form/User/Hook/useUser";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
@@ -9,14 +9,20 @@ type AuthPageProps = {
 
 export default function AuthPage({ children }: AuthPageProps) {
   const { isLoggedIn, isLoading, LoginUserData } = useUser();
+  const toast = useToast();
   const navigate = useNavigate();
   useEffect(() => {
     if (!isLoading) {
       if (!isLoggedIn) {
-        navigate("/errorpage");
+        navigate("/");
+        toast({
+          title: "로그아웃",
+          description: "로그인 세션이 만료 되었습니다.",
+          status: "error",
+        });
       }
     }
-  }, [isLoading, isLoggedIn, LoginUserData, navigate]);
+  }, [isLoading, isLoggedIn, LoginUserData, navigate, toast]);
   if (!isLoading) {
     return <>{children}</>;
   } else {
